5ce7ca9360d6db0b2b75bcbad1899b3b7d38dd7c
antonioarmanihorta <antonio.armani.horta@gmail.com>
2026-04-15 11:50:00 -0300

    sefsefesf

--- Programa.java
+++ Programa.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class Programa {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner()    
     }
 }
