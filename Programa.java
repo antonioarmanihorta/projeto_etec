b13897dfbe19d598946e28efae15f8eae95115e2
antonioarmanihorta <antonio.armani.horta@gmail.com>
2026-04-15 12:12:00 -0300

    Main

--- Programa.java
+++ Programa.java
@@ -4,5 +4,7 @@ public class Programa {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in)
         System.out.println("Bem vindo ao programa!");
+        System.out.println("Realize o login com seu usuário ou senha:");
+        
     }
 }
